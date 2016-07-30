6cecc4b24c98b0bf7271f4fb432945d95d329342
James Abels <jamesrabels@gmail.com>
2016-07-30 14:20:01 -0400

    Add error checking to fetch request

--- src/actions/test_actions.js
+++ src/actions/test_actions.js
@@ -1,15 +1,24 @@
-/// Define Type 
+// Define Type 
 export const DATA = 'DATA'; 
 
 // Test Actions
 export default TestActions = {};
 
-
 // GET DATA  
 TestActions.GET_DATA = function (url) {
+    
     return (dispatch) => {
+        
         fetch(url, {
             method: 'GET'
+        })
+        .then((response) => {
+            
+            if (!response.ok) {
+                throw Error("HTTP REQUEST FAILED", response.statusText);
+            }
+            
+            return response;
         })
         .then((response) => response.json())
         .then((responseData) => {
@@ -25,5 +34,6 @@ TestActions.GET_DATA = function (url) {
 
         })
         .done();
+
     }
 }
